6635c47a601b4275ae252bf74ce690dc947181a2
Yao遥遥无期 <2602875449@qq.com>
2022-04-25 18:47:50 +0800

    8.0第2次课

--- 8-4 3/js/js.js
+++ 8-4 3/js/js.js
@@ -4,7 +4,8 @@
 function addb() {
     const ne = document.getElementById('ne').value;
     const vip = document.getElementById('vip').value;
-    const phone =  $('#phone').val();
+    const phone = document.getElementById('phone').value;
+    // const phone =  $('#phone').val();
     var vipnum = /^[0-9]{1,12}$/    //正则表达式
     if ((ne.length == 0) || (vip.length == 0) ||(phone.length == 0)) {
         alert('请输入完整信息');
@@ -22,12 +23,12 @@ function addb() {
         var td2 = document.createElement('td');
         tr.appendChild(td2);
         td2.innerHTML = vip;
-        const td4 = document.createElement('td');
-        tr.appendChild(td4);
-        td4.innerHTML = phone;
-        var td3 = document.createElement('td');
+        const td3 = document.createElement('td');
         tr.appendChild(td3);
-        td3.innerHTML = "<input type='button' onclick='dele(this)' value='删除'/>";
+        td3.innerHTML = phone;
+        var td4 = document.createElement('td');
+        tr.appendChild(td4);
+        td4.innerHTML = "<input type='button' onclick='dele(this)' value='删除'/>";
     }
 }
 function dele(c) {
